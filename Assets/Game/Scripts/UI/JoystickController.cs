b3046e697ec303bd50100f9cfe7fb815f56087e0
Vitali <37334443+v1t3@users.noreply.github.com>
2021-10-04 23:58:49 +0300

    Добавлен поворот управления

---
+++ Assets/Game/Scripts/UI/JoystickController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class JoystickController : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraTransform;
+        [SerializeField] private Transform gameFieldTransform;
+        [SerializeField] private Transform joystickTransform;
+
+        private void Update()
+        {
+            var rot = gameFieldTransform.position - cameraTransform.position;
+            Quaternion toField = Quaternion.LookRotation(rot);
+            
+            // Debug.Log("rot " + rot);
+            // Debug.Log("toField " + toField);
+
+            joystickTransform.localRotation = new Quaternion(0,0,toField.y,toField.w);
+        }
+    }
+}
