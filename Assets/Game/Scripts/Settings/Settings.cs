ea8c3d54d2b95bfc69f2dbaa8d7841ff407eb217
Vitali <37334443+v1t3@users.noreply.github.com>
2021-11-28 21:17:33 +0300

    Добавлена настройка включения сетки

---
+++ Assets/Game/Scripts/Settings/Settings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Settings
+{
+    public class Settings : MonoBehaviour
+    {
+        [SerializeField] private GameObject grid;
+        
+        private bool _isGridEnabled;
+
+        public void ToggleGrid(bool value)
+        {
+            _isGridEnabled = value;
+            grid.SetActive(_isGridEnabled);
+        }
+    }
+}
